f69d8c2f6457ba505c7988717cb6c645c9b4fc8d
tasyaswantara <tasyaswantara@gmail.com>
2024-03-03 16:42:46 +0700

    initial commit

---
+++ Menu.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.CetakFilkom;
+
+/**
+ *
+ * @author victus joe
+ */
+public class Menu {
+    String tipeMenu;
+    String namaMenu;
+    int hargaSatuan;
+
+    public Menu(String tipeMenu, String namaMenu, int hargaSatuan) {
+        this.tipeMenu = tipeMenu;
+        this.namaMenu = namaMenu;
+        this.hargaSatuan = hargaSatuan;
+    }
+}
